0239777d5a46305b83e417f566d6736d066354b5
xxxMichael <mayckolchavez27@gmail.com>
2025-10-07 12:22:18 -0500

    Agregar implementación de IDatabaseConnection y DatabaseService, y definir servicio de autenticación en auth.proto

---
+++ MS.Autenticacion/Domain/Interfaces/IDatabaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace MS.Autenticacion.Domain.Interfaces
+{
+    public interface IDatabaseConnection
+    {
+        Task<bool> TestConnectionAsync();
+        Task<DataTable> ExecuteQueryAsync(string query, MySqlParameter[]? parameters = null);
+        Task<int> ExecuteNonQueryAsync(string query, MySqlParameter[]? parameters = null);
+    }
+}
