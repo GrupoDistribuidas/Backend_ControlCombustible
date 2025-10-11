e7eb55b98f839dd38f0323611c24c006f0092bd2
DavidGR21 <davidgiler21@gmail.com>
2025-10-11 11:24:41 -0500

    Se cambio el dto para habilitar el actualizar

--- MS.Vehiculos/Application/DTOs/CrearVehiculoDto.cs
+++ MS.Vehiculos/Application/DTOs/CrearVehiculoDto.cs
@@ -11,4 +11,18 @@ namespace MS.Vehiculos.Application.DTOs
         public decimal ConsumoCombustibleKm { get; set; }
         public decimal CapacidadCombustible { get; set; }
     }
+
+    public class ActualizarVehiculoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Placa { get; set; } = null!;
+        public string Marca { get; set; } = null!;
+        public string Modelo { get; set; } = null!;
+        public int TipoMaquinariaId { get; set; }
+        public string Disponible { get; set; } = "Disponible";
+        public decimal ConsumoCombustibleKm { get; set; }
+        public decimal CapacidadCombustible { get; set; }
+        public bool? Estado { get; set; } = null;
+    }
 }
