7b43865280537e88513dce0fd8656d2c6226a1b7
DavidGR21 <davidgiler21@gmail.com>
2025-10-12 08:22:14 -0500

    Se crearon las interfaces para Choferes y para tipo de maquinaria

---
+++ MS.Choferes/Domain/Interfaces/IChoferRepository.cs
@@ -0,0 +1,18 @@
+using MS.Choferes.Domain.Entities;
+using System.Collections.Generic;
+
+namespace MS.Choferes.Domain.Interfaces
+{
+    public interface IChoferRepository
+    {
+        Task<int> CreateAsync(Chofer chofer);
+        Task<Chofer?> GetByIdAsync(int id);
+        Task<IEnumerable<Chofer>> GetAllAsync();
+        Task<Chofer?> GetByIdentificacionAsync(string identificacion);
+        Task<int> UpdateAsync(Chofer chofer, bool? estado = null);
+    Task<int> UpdateEstadoAsync(int id, bool estado);
+    Task<int> UpdateDisponibilidadAsync(int id, bool disponible);
+        Task<IEnumerable<Chofer>> SearchAsync(MS.Choferes.Application.DTOs.ChoferFilterDto filter);
+        Task<IEnumerable<Chofer>> SearchByTermAsync(string term);
+    }
+}
